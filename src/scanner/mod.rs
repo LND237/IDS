3a82225f085796cb884f58813db3704a34a5bcc8
boaz sarel <boaz.sarel@gmail.com>
2023-12-14 23:30:05 +0200

    Adding a field of attack_name to Scanner

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -10,15 +10,33 @@ pub mod scanner {
         fn check_packets(packets: Vec<SinglePacket>) -> bool;
     }
     pub struct Scanner{
+        attack_name: String,
         ip: IP
     }
 
     impl Scanner{
-        ///Default C'tor of struct Scanner
-        ///Input: an IP structure- the IP for the Scanner.
+        ///C'tor of struct Scanner
+        ///Input: an IP structure- the IP for the Scanner and
+        /// a String variable- the name of the attack to scan.
         /// Output: The Scanner object Structure.
-        pub fn new(ip: IP) -> Self{
-            return Scanner{ip: IP::copy(ip)};
+        pub fn new(ip: IP, attack_name: String) -> Self{
+            return Scanner{ip: IP::copy(ip), attack_name: attack_name.clone()};
+        }
+
+        ///The function gets the name of the attack which the
+        /// scanner scans.
+        /// Input: self reference(Scanner).
+        /// Output: The name of the attack of the Scanner.
+        pub fn get_name(&self) -> String{
+            return self.attack_name.clone();
+        }
+
+        ///The function gets the IP for the
+        /// scanner.
+        /// Input: self reference(Scanner).
+        /// Output: The IP to scan.
+        pub fn get_ip(&self) -> IP{
+            return IP::copy(self.ip);
         }
     }
 }
