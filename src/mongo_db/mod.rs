aa9b65e5cfdf314154a432e972183f054229fd67
boaz sarel <boaz.sarel@gmail.com>
2024-01-25 18:39:36 +0200

    Adding a copy function to AttackData struct

--- src/mongo_db/mod.rs
+++ src/mongo_db/mod.rs
@@ -65,6 +65,10 @@ pub mod mongo_db{
             return format!("{{'ip': '{}', 'name': '{}', 'date': '{}'}}", self.ip_attacker.get_ip(),
                            self.attack_name.clone(), self.date.to_string());
         }
+
+        pub fn copy(&self) -> Self{
+            return Self{ip_attacker: self.ip_attacker.copy(), attack_name: self.attack_name.clone(), date: self.date.clone()};
+        }
     }
 
     //MongoDB Functions
