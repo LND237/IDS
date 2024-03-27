312359445410e3c23efd99953ca4c26e9fd0d8bd
boaz sarel <boaz.sarel@gmail.com>
2024-03-27 19:01:38 +0200

    Adding the AttackData class

---
+++ Client/AttackData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class AttackData
+    {
+        [JsonPropertyName("ip")]
+        private string ip;
+        [JsonPropertyName("name")]
+        private string name;
+        [JsonPropertyName("date")]
+        private string date;
+
+        public AttackData(string ip, string name, string date)
+        {
+            this.ip = ip;
+            this.name = name;
+            this.date = date;
+        }
+
+        public string GetIP()
+        {
+            return ip;
+        }
+
+        public string GetName() 
+        {
+            return name;
+        }
+
+        public string GetDate() 
+        {
+            return date;
+        }
+    }
+}
--- Client/Communicator.cs
+++ Client/Communicator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WpfApp1.Models.Ip;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,18 @@ namespace Client
     internal class Communicator
     {
         private const int PORT_NUM = 50001;
+        private readonly IP ip;
+
+        public Communicator()
+        {
+            this.ip = new IP("127.0.0.1");
+        }
+
+        public AttackData GetMessageServer()
+        {
+
+            return new AttackData();
+        }
+
     }
 }
